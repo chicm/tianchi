d509cf1f477c6cef7c7a4a2b14f6d453f445745e
chicm <idv2005@gmail.com>
2017-07-06 02:40:15 +0800

    batchnorm

--- train_unet.py
+++ train_unet.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, BatchNormalization
 from keras.optimizers import Adam
 from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
@@ -32,48 +32,66 @@ def dice_coef_np(y_true,y_pred):
     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
 def dice_coef_loss(y_true, y_pred):
-    return -dice_coef(y_true, y_pred)
+    return 1-dice_coef(y_true, y_pred)
 
 
 def get_unet():
     inputs = Input((1,img_rows, img_cols))
     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
+    conv1 = BatchNormalization(axis=1)(conv1)
     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+    conv1 = BatchNormalization(axis=1)(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
+    conv2 = BatchNormalization(axis=1)(conv2)
     conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+    conv2 = BatchNormalization(axis=1)(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
     conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
+    conv3 = BatchNormalization(axis=1)(conv3)
     conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+    conv3 = BatchNormalization(axis=1)(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
     conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
+    conv4 = BatchNormalization(axis=1)(conv4)
     conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
+    conv4 = BatchNormalization(axis=1)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
+    conv5 = BatchNormalization(axis=1)(conv5)
     conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
+    conv5 = BatchNormalization(axis=1)(conv5)
 
     up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
     conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(up6)
+    conv6 = BatchNormalization(axis=1)(conv6)
     conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv6)
+    conv6 = BatchNormalization(axis=1)(conv6)
 
     up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
     conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up7)
+    conv7 = BatchNormalization(axis=1)(conv7)
     conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv7)
+    conv7 = BatchNormalization(axis=1)(conv7)
 
     up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
     conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
+    conv8 = BatchNormalization(axis=1)(conv8)
     conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv8)
+    conv8 = BatchNormalization(axis=1)(conv8)
 
     up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
     conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
+    conv9 = BatchNormalization(axis=1)(conv9)
     conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
+    conv9 = BatchNormalization(axis=1)(conv9)
 
     conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv9)
-
+    
     model = Model(input=inputs, output=conv10)
 
     model.compile(optimizer=Adam(lr=0.001), loss=dice_coef_loss, metrics=[dice_coef])
@@ -81,6 +99,8 @@ def get_unet():
     return model
     
 def scheduler(epoch):
+    if epoch < 5:
+        return 0.005
     if epoch < 10:
         return 0.001
     if epoch < 20:
@@ -100,11 +120,22 @@ def train_and_predict(use_existing):
     #imgs_test = np.load(working_path+"testImages.npy").astype(np.float32)
     #imgs_mask_test_true = np.load(working_path+"testMasks.npy").astype(np.float32)
     
-    mean = np.mean(imgs_train)  # mean for data centering
-    std = np.std(imgs_train)  # std for data normalization
+    #mean = np.mean(imgs_train)  # mean for data centering
+    #std = np.std(imgs_train)  # std for data normalization
 
-    imgs_train -= mean  # images should already be standardized, but just in case
-    imgs_train /= std
+    #minvalue = np.min(imgs_train)
+    #maxvalue = np.max(imgs_train)
+    #imgs_train -= minvalue
+    #imgs_train /= (maxvalue-minvalue)
+
+    #imgs_val -= minvalue
+    #imgs_val /= (maxvalue-minvalue)
+
+    imgs_mask_train *= 255
+    imgs_mask_val *= 255
+
+    #imgs_train -= mean  # images should already be standardized, but just in case
+    #imgs_train /= std
 
     print('-'*30)
     print('Creating and compiling model...')
@@ -116,6 +147,7 @@ def train_and_predict(use_existing):
     # Should we load existing weights? 
     # Set argument for call to train_and_predict to true at end of script
     if use_existing:
+        print('loading weight...')
         model.load_weights('./unet.hdf5')
         
     # 
@@ -157,4 +189,4 @@ def train_and_predict(use_existing):
     print("Mean Dice Coeff : ",mean)
 '''
 if __name__ == '__main__':
-    train_and_predict(False)
+    train_and_predict(True)
